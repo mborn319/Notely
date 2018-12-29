9aba06ed1ae1ccef2babdbd61ff3090596b65669
Michael Born <mborn319@gmail.com>
2018-12-29 12:25:26 -0500

    Begin basic NoteForm component for adding a note.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NoteForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NoteForm from './components/NoteForm.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NoteForm
   }
 }
 </script>
---
+++ src/components/NoteForm.vue
@@ -0,0 +1,22 @@
+<template>
+<form>
+	<div class="field">
+		<label for="note" class="label">Note</label>
+		<div class="control"><textarea class="textarea"></textarea></div>
+	</div>
+	<div class="field is-grouped">
+		<div class="control"><button type="submit" class="button is-link">Add Note</button></div>
+		<div class="control"><button type="reset" class="button is-text">Reset</button></div>
+	</div>
+</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
